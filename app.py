b80181d3d9031bac600d928cd65379084d2eb857
John Strunk <developer@jrstrunk.com>
2024-09-30 09:25:06 -0400

    Move logic into main route function

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 app = flask.Flask(__name__)
 
 @app.route('/detect_faces', methods=['POST'])
-def hash_image():
+def detect_faces():
     if 'image' not in flask.request.files:
         return flask.jsonify({'error': 'No image file provided'}), 400
     
@@ -15,43 +15,31 @@ def hash_image():
     nparr = np.frombuffer(image_bytes, np.uint8)
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     
-    # Compute the perceptual hash of the image
-    faces = retinaface_extract_faces(image)
+    faces = []
+
+    # Validate image shape
+    if len(image.shape) != 3 or np.prod(image.shape) == 0:
+        raise ValueError("Input image needs to have 3 channels at must not be empty.")
+
+    obj = RetinaFace.detect_faces(
+        img_path=image, threshold=0.9, model=None, allow_upscaling=True
+    )
+
+    if isinstance(obj, dict): 
+        for _, identity in obj.items():
+            facial_area = identity["facial_area"]
+
+            left = facial_area[0]
+            top = facial_area[1]
+            width = facial_area[2] - left
+            height = facial_area[3] - top
+
+            faces.append(
+                {"ltwh": f"{left},{top},{width},{height}"}
+            )
     
     return flask.jsonify({'face_bounds': str(faces)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
-
-# A modification of the retinaface package's extract_faces function so that
-# it will return the cropped faces and also the cooordinates of the face
-def retinaface_extract_faces(
-    img: np.ndarray,
-) -> list:
-    resp = []
-
-    # Validate image shape
-    if len(img.shape) != 3 or np.prod(img.shape) == 0:
-        raise ValueError("Input image needs to have 3 channels at must not be empty.")
-
-    obj = RetinaFace.detect_faces(
-        img_path=img, threshold=0.9, model=None, allow_upscaling=True
-    )
-
-    if not isinstance(obj, dict):
-        return resp
-
-    for _, identity in obj.items():
-        facial_area = identity["facial_area"]
-
-        left = facial_area[0]
-        top = facial_area[1]
-        width = facial_area[2] - left
-        height = facial_area[3] - top
-
-        resp.append(
-            {"ltwh": f"{left},{top},{width},{height}"}
-        )
-
-    return resp
